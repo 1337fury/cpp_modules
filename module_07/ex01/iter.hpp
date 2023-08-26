49a4676e1890eeee2e5ac4531bd15bfa53f85978
1337fury <elouardiabdelhalim@gmail.com>
2023-08-26 15:30:06 +0100

    add comments

--- module_07/ex01/iter.hpp
+++ module_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 13:55:54 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/08/06 14:39:58 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/08/22 18:37:25 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 
 #include <stdlib.h>
 
+// Template function that iterates over an array and applies a function to each element
 template <class T, class F>
 void	iter(T* array, size_t length, F function)
 {
 	for (size_t i = 0; i < length; i++)
-		function(array[i]);
+        function(array[i]); // Apply the function to the current element
 }
 
 #endif
